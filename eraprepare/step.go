126c9b3284593c9d2e9f9bbf84623e8f5494cdc6
Andrea Parodi <andrea@parro.it>
2020-03-25 18:02:21 +0100

    finished preparation of era data

--- eraprepare/step.go
+++ eraprepare/step.go
@@ -14,7 +14,7 @@ func parseDate(dt int32) time.Time {
 	return time.Unix(int64(dt)*60*60-int64(2208988800), 0)
 }
 
-func createOutputFile(date string) netcdf.Dataset {
+func createOutputFile(date string, inputData netcdf.Dataset) netcdf.Dataset {
 	eraOutFile := "data/era5-merged-" + date + ".nc"
 	eraOutData, err := netcdf.CreateFile(eraOutFile, netcdf.NETCDF4)
 	if err != nil {
@@ -22,19 +22,36 @@ func createOutputFile(date string) netcdf.Dataset {
 		panic(err)
 	}
 
-	lonDim, err := eraOutData.AddDim("longitude", uint64(3600))
+	// dimensions lengths
+	lonLen := uint64(3600)
+	latLen := uint64(1801)
+	timeLen := uint64(24)
+
+	// create dimensions
+	lonDim, err := eraOutData.AddDim("longitude", lonLen)
 	if err != nil {
 		panic(err)
 	}
-	latDim, err := eraOutData.AddDim("latitude", uint64(1801))
+	latDim, err := eraOutData.AddDim("latitude", latLen)
 	if err != nil {
 		panic(err)
 	}
-	timeDim, err := eraOutData.AddDim("time", uint64(24))
+	timeDim, err := eraOutData.AddDim("time", timeLen)
 	if err != nil {
 		panic(err)
 	}
 
+	// input data geo coord var
+	inputLonVar, err := inputData.Var("longitude")
+	if err != nil {
+		panic(err)
+	}
+	inputLatVar, err := inputData.Var("latitude")
+	if err != nil {
+		panic(err)
+	}
+
+	// create lat and lon variables
 	lonVar, err := eraOutData.AddVar("longitude", netcdf.FLOAT, []netcdf.Dim{lonDim})
 	if err != nil {
 		panic(err)
@@ -57,6 +74,29 @@ func createOutputFile(date string) netcdf.Dataset {
 		panic(err)
 	}
 
+	// fill lat and lon variables with same values
+	// as input dataset
+	inputLatVarData := make([]float32, latLen)
+	err = inputLatVar.ReadFloat32s(inputLatVarData)
+	if err != nil {
+		panic(err)
+	}
+	err = latVar.WriteFloat32s(inputLatVarData)
+	if err != nil {
+		panic(err)
+	}
+
+	inputLonVarData := make([]float32, lonLen)
+	err = inputLonVar.ReadFloat32s(inputLonVarData)
+	if err != nil {
+		panic(err)
+	}
+	err = lonVar.WriteFloat32s(inputLonVarData)
+	if err != nil {
+		panic(err)
+	}
+
+	// create time variable
 	timeVar, err := eraOutData.AddVar("time", netcdf.INT, []netcdf.Dim{timeDim})
 	if err != nil {
 		panic(err)
@@ -71,6 +111,26 @@ func createOutputFile(date string) netcdf.Dataset {
 		panic(err)
 	}
 
+	// fill time variable
+	dt, err := time.Parse("20060102", date)
+	if err != nil {
+		panic(err)
+	}
+
+	hoursFrom1900AtMidnight := int32(dt.Unix()/(60*60) + 613608)
+	inputTimeVarData := make([]int32, lonLen)
+
+	for h := int32(0); h < 24; h++ {
+		inputTimeVarData[h] = h + hoursFrom1900AtMidnight
+	}
+
+	err = timeVar.WriteInt32s(inputTimeVarData)
+	if err != nil {
+		panic(err)
+	}
+
+	// create u10 var
+
 	threeDims := []netcdf.Dim{timeDim, latDim, lonDim}
 	u10Var, err := eraOutData.AddVar("u10", netcdf.FLOAT, threeDims)
 	if err != nil {
@@ -83,6 +143,8 @@ func createOutputFile(date string) netcdf.Dataset {
 		panic(err)
 	}
 
+	// create v10 var
+
 	v10Var, err := eraOutData.AddVar("v10", netcdf.FLOAT, threeDims)
 	if err != nil {
 		panic(err)
@@ -93,6 +155,8 @@ func createOutputFile(date string) netcdf.Dataset {
 	if err := v10Var.Attr("long_name").WriteBytes([]byte("10 metre V wind component")); err != nil {
 		panic(err)
 	}
+
+	// create d2m var
 
 	d2mVar, err := eraOutData.AddVar("d2m", netcdf.FLOAT, threeDims)
 	if err != nil {
@@ -105,6 +169,8 @@ func createOutputFile(date string) netcdf.Dataset {
 		panic(err)
 	}
 
+	// create t2m var
+
 	t2mVar, err := eraOutData.AddVar("t2m", netcdf.FLOAT, threeDims)
 	if err != nil {
 		panic(err)
@@ -115,6 +181,8 @@ func createOutputFile(date string) netcdf.Dataset {
 	if err := t2mVar.Attr("long_name").WriteBytes([]byte("2 metre temperature")); err != nil {
 		panic(err)
 	}
+
+	// create elevation var
 
 	elevationVar, err := eraOutData.AddVar("elevation", netcdf.FLOAT, threeDims)
 	if err != nil {
@@ -141,64 +209,63 @@ func Run(date, dateBefore string, domain *core.Domain) {
 	}
 	defer eraData.Close()
 
-	// time
-	timeV, err := eraData.Var("time")
-	if err != nil {
-		panic(err)
-	}
-
-	time, err := netcdf.GetInt32s(timeV)
-	if err != nil {
-		panic(err)
-	}
-
-	timeDims, err := timeV.LenDims()
-	if err != nil {
-		panic(err)
-	}
-
-	// latitude
-	latitudeV, err := eraData.Var("latitude")
-	if err != nil {
-		panic(err)
-	}
-	/*
-		latitude, err := netcdf.GetInt32s(latitudeV)
-		if err != nil {
-			panic(err)
-		}
-	*/
-	latitudeDims, err := latitudeV.LenDims()
-	if err != nil {
-		panic(err)
-	}
-
-	// longitude
-	longitudeV, err := eraData.Var("longitude")
-	if err != nil {
-		panic(err)
-	}
-	/*
-		longitude, err := netcdf.GetInt32s(longitudeV)
-		if err != nil {
-			panic(err)
-		}
-	*/
-	longitudeDims, err := longitudeV.LenDims()
-	if err != nil {
-		panic(err)
-	}
-
-	eraOutData := createOutputFile(date)
+	eraOutData := createOutputFile(date, eraData)
 	defer eraOutData.Close()
 
-	outTimeIdx := uint64(0)
-	tot := timeDims[0] * latitudeDims[0] * longitudeDims[0]
-	runningIdx := uint64(0)
-	lastProgress := -1.0
+	copyVar(0, eraData, eraOutData, "d2m", 0)
+	copyVar(1, eraData, eraOutData, "t2m", 0)
+	copyVar(2, eraData, eraOutData, "u10", -273.15)
+	copyVar(3, eraData, eraOutData, "v10", -273.15)
+
+	fmt.Printf("\033[F")
+	fmt.Printf("\033[K")
+	fmt.Printf("[4] ✔️ Prepared Era5 single file: `%s`\n", targetFile)
+
+}
+
+func copyVar(idxVar int, eraData, eraOutData netcdf.Dataset, varName string, deltaConversion float64) {
+	inVar, err := eraData.Var(varName)
+	if err != nil {
+		panic(err)
+	}
+
+	varLen, err := inVar.Len()
+	if err != nil {
+		panic(err)
+	}
+
+	varData := make([]int16, varLen)
+	varDataOut := make([]float32, varLen)
+
+	err = inVar.ReadInt16s(varData)
+	if err != nil {
+		panic(err)
+	}
+
+	outVar, err := eraOutData.Var(varName)
+	if err != nil {
+		panic(err)
+	}
+
+	scaleFactorVec := []float64{0}
+	err = inVar.Attr("scale_factor").ReadFloat64s(scaleFactorVec)
+	if err != nil {
+		panic(err)
+	}
+
+	addOffsetVec := []float64{0}
+	err = inVar.Attr("add_offset").ReadFloat64s(addOffsetVec)
+	if err != nil {
+		panic(err)
+	}
+
+	scaleFactor := scaleFactorVec[0]
+	addOffset := addOffsetVec[0]
+	idx := uint64(0)
+	lastProgress := float64(0)
 
 	reportProgress := func() {
-		progress := math.Round(float64(runningIdx)*100*100/float64(tot)) / 100
+		progress := float64(idxVar)*25.0 + math.Round(float64(idx)*100*100/float64(varLen))/100/4
 		if progress != lastProgress {
 			fmt.Printf("\033[F")
 			fmt.Printf("\033[K")
@@ -207,96 +274,13 @@ func Run(date, dateBefore string, domain *core.Domain) {
 		}
 	}
 
-	for t := uint64(0); t < uint64(timeDims[0]); t++ {
-		dt := parseDate(time[t])
-		if dt.Format("20060102") != date {
-			runningIdx += latitudeDims[0] * longitudeDims[0]
-			reportProgress()
-			continue
-		}
-
-		for lat := uint64(0); lat < uint64(latitudeDims[0]); lat++ {
-			for lon := uint64(0); lon < uint64(longitudeDims[0]); lon++ {
-				latValue, err := latitudeV.ReadFloat32At([]uint64{lat})
-				if err != nil {
-					panic(err)
-				}
-				lonValue, err := longitudeV.ReadFloat32At([]uint64{lon})
-				if err != nil {
-					panic(err)
-				}
-				if float64(latValue) < domain.MinLat || float64(latValue) > domain.MaxLat ||
-					float64(lonValue) < domain.MinLon || float64(lonValue) > domain.MaxLon {
-					writeEmptyVar(eraOutData, "u10", t, lat, lon, outTimeIdx)
-					writeEmptyVar(eraOutData, "v10", t, lat, lon, outTimeIdx)
-					writeEmptyVar(eraOutData, "d2m", t, lat, lon, outTimeIdx)
-					writeEmptyVar(eraOutData, "t2m", t, lat, lon, outTimeIdx)
-					runningIdx++
-					reportProgress()
-					continue
-				}
-
-				copyVar(eraData, eraOutData, "u10", t, lat, lon, outTimeIdx, 0)
-				copyVar(eraData, eraOutData, "v10", t, lat, lon, outTimeIdx, 0)
-				copyVar(eraData, eraOutData, "d2m", t, lat, lon, outTimeIdx, -273.15)
-				copyVar(eraData, eraOutData, "t2m", t, lat, lon, outTimeIdx, -273.15)
-				runningIdx++
-				reportProgress()
-			}
-		}
-		outTimeIdx++
+	for idx = (0); idx < varLen; idx++ {
+		varDataOut[idx] = float32(float64(varData[idx])*scaleFactor + addOffset + deltaConversion)
+		reportProgress()
 	}
 
-	fmt.Printf("\033[F")
-	fmt.Printf("\033[K")
-	fmt.Printf("[4] ✔️ Prepared Era5 single file: `%s`\n", targetFile)
+	err = outVar.WriteFloat32s(varDataOut)
 
-}
-
-func copyVar(eraData, eraOutData netcdf.Dataset, varName string, t, lat, lon uint64, outTimeIdx uint64, deltaConversion float64) {
-	inVar, err := eraData.Var(varName)
-	if err != nil {
-		panic(err)
-	}
-
-	outVar, err := eraOutData.Var(varName)
-	if err != nil {
-		panic(err)
-	}
-
-	val, err := inVar.ReadInt16At([]uint64{t, lat, lon})
-	if err != nil {
-		panic(err)
-	}
-
-	scaleFactor := []float64{0}
-	err = inVar.Attr("scale_factor").ReadFloat64s(scaleFactor)
-	if err != nil {
-		panic(err)
-	}
-
-	addOffset := []float64{0}
-	err = inVar.Attr("add_offset").ReadFloat64s(scaleFactor)
-	if err != nil {
-		panic(err)
-	}
-
-	valF := float32(float64(val)*scaleFactor[0] + addOffset[0] + deltaConversion)
-
-	err = outVar.WriteFloat32At([]uint64{outTimeIdx, lat, lon}, valF)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func writeEmptyVar(eraOutData netcdf.Dataset, varName string, t, lat, lon uint64, outTimeIdx uint64) {
-
-	outVar, err := eraOutData.Var(varName)
-	if err != nil {
-		panic(err)
-	}
-
-	err = outVar.WriteFloat32At([]uint64{outTimeIdx, lat, lon}, 0)
 	if err != nil {
 		panic(err)
 	}
